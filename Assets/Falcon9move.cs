dd0eb5d63985ccb9f58ad8340555694c13250d32
sw0101 <sweidenthal23@us.edu>
2021-06-04 13:06:29 -0400

    Final (Yay!!)

--- Assets/Falcon9move.cs
+++ Assets/Falcon9move.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
+
 public class Falcon9move : MonoBehaviour
 {
     public float speed = 1;
@@ -11,12 +13,27 @@ public class Falcon9move : MonoBehaviour
 
     private Rigidbody rigidbody;
 
+    // Particle systems
+    public GameObject T1;
+    public GameObject T2;
+    public GameObject T3;
+    public GameObject Steam;
+    public GameObject Steam1;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        T1.SetActive(false); // Disabled Thrustor 1 Left (T1)
+        T2.SetActive(false); // Disabled Thrustor 2 Middle (T2)
+        T3.SetActive(false); // Disabled Thrustor 2 Middle (T2)
+
+        
+
+
 
         start_time = Time.time;
     }
@@ -32,10 +49,20 @@ public class Falcon9move : MonoBehaviour
 
         float current_time = Time.time;
 
+    // This begins the rocket "launch" sequence after 24 seconds (to match recording.)
+    // Note: when game is started, all 3 thrustors are disabled. They are enabled once launch happens.
+    
+        if((current_time - start_time) > 24) {// This controls time
+            rigidbody.AddForce(movement * speed); // This is the thing that actually moves the rocket (add force)
+            Steam.SetActive(false); // Disables pre-launch element (Steam)
+            Steam1.SetActive(false); // Disables pre-launch element (Steam1)
+        }
 
-        if((current_time - start_time) > 25)
-            rigidbody.AddForce(movement * speed);
-            
-        
+        if((current_time - start_time) > 20) {// This controls time
+            rigidbody.AddForce(movement * speed); // This is the thing that actually moves the rocket (add force)
+            T1.SetActive(true); // Enables Thrustor 1 Left (T1)
+            T2.SetActive(true); // Enables Thrustor 2 Middle (T2)
+            T3.SetActive(true); // Enables Thrustor 2 Middle (T2)
+        }
     }
 }
